99da1f63e71629cc2cacedc5008580350d3f34f9
zwq <zwq@qq.com>
2019-05-15 20:17:00 +0800

    第二次提交,修改了歌词

--- src/main/java/com/qf/main/demo.java
+++ src/main/java/com/qf/main/demo.java
@@ -9,9 +9,10 @@ public class demo {
 
     public void test1(){
         System.out.println("当你走进这欢乐场");
-        System.out.println("当你走进这欢乐场");
-        System.out.println("当你走进这欢乐场");
-        System.out.println("当你走进这欢乐场");
-        System.out.println("当你走进这欢乐场");
+        System.out.println("忘记所有的梦与想");
+        System.out.println("各色的脸上各色的妆");
+        System.out.println("没人记得你的摸样");
+        System.out.println("三巡走过你在角落");
+        System.out.println("固执的唱着苦涩的歌");
     }
 }
